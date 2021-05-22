a07d0268ff327a34e1bbc065e786c1e0b6467b9e
kevin-contreras <80024551+kevincontreras-2003@users.noreply.github.com>
2021-05-21 20:47:29 -0500

    Donde se encuentran los códigos
    Donde se encuentran los códigos de Bootstrap icons, datatables, y charjs.
    
    -Bootstrap icons se encuentra como Iconos.js
    
    -Datatables se encuentra  como Datatable.js
    
    -Chartjs se encuentra como Chart.js
    
    
    POR ULTIMO Complementaria.txt EN ESTE BLOG DE TEXTO ESTAN TODAS LAS DOCUMENTACIONES PARA LOS TEMAS, SE ENCUENTRA COMO LA INSTALACION Y SE ENCUENTRA  LA PAGINA PARA LOS CODIGOS DE LAS TABLAS, ICONOS O BARRAS QUE QUIERAS UTILIZAR

---
+++ Iconos.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {File,ArrowRight} from 'react-bootstrap-icons';
+
+export default function Iconos (){
+    return (
+        <div>
+        <center>  <File size={96}   />
+        <br></br>
+        <ArrowRight size={96}   /></center>  
+        </div>
+    );
+}
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Iconos from './productos/component/Iconos';
+import Datatable from './productos/component/Datatable';
+import Chart from './productos/component/Chart';
+
+
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(
+   
+    //<Iconos />,
+    //<Datatable />,
+   <Chart />, 
+
+  document.getElementById('root')
+);
+
